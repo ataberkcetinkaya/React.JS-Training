574571d69f6481260b0a4275a42727b2f0b1546b
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-08-30 06:40:32 -0300

    Creating The Weather App

---
+++ weatherapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WeatherService from './services/WeatherService';
+
+function App() {
+
+  return (
+      <>
+        <WeatherService></WeatherService>
+      </>
+  );
+}
+
+export default App;
---
+++ weatherapp/src/services/WeatherService.js
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from 'react'
+import axios from 'axios'
+import '../App.css';
+
+const WeatherService = () => {
+  //openweathermap.org
+  const url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const [weather, setWeather] = useState({});
+  const inputRef = useRef(null);
+
+  const getWeather = async () => {
+      const { data } = await axios.get(`${url}${inputRef.current.value}&appid=${apiKey}`) //wait for data to reach (async and await)
+      setWeather(data);
+  }
+
+  return (
+    <>
+      <div className="flex justify-center mt-5">
+        <input ref={inputRef} type="text" className='w-64 h-10 border-solid border-2 border-black mr-3' placeholder='Type city...'></input>
+        <button onClick={() => getWeather()} className='w-24 border-solid border-2 border-black'>Search</button>
+      </div>
+      <div className="flex flex-col text-center mt-5 text-2xl">
+        <h3 className='mt-5'>{weather.name}</h3>
+        <h3 className='mt-2'>{weather?.main?.temp}</h3>
+      </div>
+    </>
+  )
+}
+
+export default WeatherService
