6eaf6c0f79837d6f17a108856e9d04c97a090fe5
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-08-31 03:46:08 -0300

    Weather Status & Metric Celsius

--- weatherapp/src/services/WeatherService.js
+++ weatherapp/src/services/WeatherService.js
@@ -11,10 +11,12 @@ const WeatherService = () => {
   const inputRef = useRef(null);
 
   const getWeather = async () => {
-      const { data } = await axios.get(`${url}${inputRef.current.value}&appid=${apiKey}`) //wait for data to reach (async and await)
+      const { data } = await axios.get(`${url}${inputRef.current.value}&appid=${apiKey}&units=metric`) //wait for data to reach (async and await)
       setWeather(data);
   }
 
+
+  //if weather exists, map through weather array and display main weather
   return (
     <>
       <div className="flex justify-center mt-5">
@@ -23,6 +25,7 @@ const WeatherService = () => {
       </div>
       <div className="flex flex-col text-center mt-5 text-2xl">
         <h3 className='mt-5'>{weather.name}</h3>
+        {weather.weather && weather.weather.map(w => <h3 className='mt-2' key={w.id}>{w.main}</h3>)} 
         <h3 className='mt-2'>{weather?.main?.temp}</h3>
       </div>
     </>
